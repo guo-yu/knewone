58e1717757de9f8d2c1cbfacf41b0e4c025e5a33
Guo Yu <i@guoyu.me>
2015-04-24 16:18:34 +0800

    Implement API things

--- libs/apis.js
+++ libs/apis.js
@@ -116,5 +116,42 @@ exports.routes = {
 
       return next(null, profile.name ? profile : null);
     }
+  },
+  things: {
+    url: '/things?page={{page|default(1)}}',
+    callback: function(err, res, html, next) {
+      if (err || res.statusCode !== 200)
+        return next(err || new Error('Network error: ' + res.statusCode), html);
+
+      var $ = cheerio.load(html);
+      var results = [];
+
+      $('.thing').each(function(){
+        var baby = {};
+        var $header = $(this).find('.cover');
+        var $content = $(this).find('.content');
+        var $info = $(this).find('.info');
+
+        if ($header.length) {
+          baby.cover = $header.find('img').attr('src')
+        }
+
+        if ($content.length) {
+          baby.name = $content.find('.title a').text()
+          baby.url = $content.find('.title a').attr('href')
+        }
+
+        if ($info.length) {
+          baby.fanciers = $info.find('.fanciers_count').text()
+          // Must signined.
+          // baby.id = $info.find('.add_to_list').attr('data-thing-id')
+        }
+
+        results.push(baby)
+      });
+
+      debug(results);
+      return next(null, results);
+    }
   }
 }
